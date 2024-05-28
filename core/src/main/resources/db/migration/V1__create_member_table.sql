b6a7ed17be58a3a1422082f74cdd23c7fcb50377
HyeongMokJeong <jhm1062@naver.com>
2024-05-28 15:48:18 +0900

    Feat : flyway sql 추가

---
+++ core/src/main/resources/db/migration/V1__create_member_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE member
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    uid      VARCHAR(100) NOT NULL,
+    platform VARCHAR(10)  NOT NULL,
+    email    VARCHAR(100),
+    role     VARCHAR(20),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
