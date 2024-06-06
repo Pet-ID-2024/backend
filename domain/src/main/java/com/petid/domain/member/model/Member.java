a88ef0997447f446b5c2fc39b2de095c3e54db22
HyeongMokJeong <jhm1062@naver.com>
2024-06-06 23:56:25 +0900

    Chore : 불필요한 Response 삭제

--- domain/src/main/java/com/petid/domain/member/model/Member.java
+++ domain/src/main/java/com/petid/domain/member/model/Member.java
@@ -3,17 +3,10 @@ package com.petid.domain.member.model;
 import com.petid.domain.type.Role;
 
 public record Member(
-<<<<<<< Updated upstream
         Long id,
         String uid,
         String platform,
         String email,
         Role role
-=======
-    String uid,
-    String platform,
-    String email,
-    Role role
->>>>>>> Stashed changes
 ) {
 }
