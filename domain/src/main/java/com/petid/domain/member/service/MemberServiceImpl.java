f40c4ceaaa80c7d8b1a86d0f53a9f323469a4ae2
Jeong HyeongMok <94634916+HyeongMokJeong@users.noreply.github.com>
2024-06-07 00:02:33 +0900

    [KAN-10] 인증 관련 누락사항 추가 (#2)
    * Chore : 불필요한 Response 삭제
    
    * Chore : 충돌 해결
    
    * Chore : 불필요한 Response 삭제
    
    * Chore : 누락 부분 추가

--- domain/src/main/java/com/petid/domain/member/service/MemberServiceImpl.java
+++ domain/src/main/java/com/petid/domain/member/service/MemberServiceImpl.java
@@ -20,7 +20,7 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
-    public MemberAuth authMember(
+    public MemberAuth saveMemberAuth(
             Member member,
             MemberAuth memberAuth
     ) {
