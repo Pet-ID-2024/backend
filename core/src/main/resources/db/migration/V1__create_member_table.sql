704ce57f8cf2c96bfb360ea73d0eb7002ad4a282
HyeongMokJeong <jhm1062@naver.com>
2024-08-07 20:14:08 +0900

    Chore : member 테이블 fcmToken 컬럼 길이 수정

--- core/src/main/resources/db/migration/V1__create_member_table.sql
+++ core/src/main/resources/db/migration/V1__create_member_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE member
     id       BIGINT AUTO_INCREMENT PRIMARY KEY,
     uid      VARCHAR(100) NOT NULL,
     platform VARCHAR(10)  NOT NULL,
-    fcm_token VARCHAR(100),
+    fcm_token VARCHAR(512),
     email    VARCHAR(100),
     role     VARCHAR(20),
     created_at TIMESTAMP NOT NULL,
