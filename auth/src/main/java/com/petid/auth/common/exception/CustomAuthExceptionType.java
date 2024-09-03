5e13fce5f4c8e1de64cee0220889e49ef68d6173
HyeongMokJeong <jhm1062@naver.com>
2024-09-03 23:07:50 +0900

    Feat : 토큰 만료 예외처리 로직 추가

--- auth/src/main/java/com/petid/auth/common/exception/CustomAuthExceptionType.java
+++ auth/src/main/java/com/petid/auth/common/exception/CustomAuthExceptionType.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum CustomAuthExceptionType {
     REGISTRATION_ID_NOT_FOUND(404, "AT-0000", "소셜 로그인 플랫폼을 찾을 수 없습니다."),
 
-    WRONG_TOKEN(403, "AT-0001", "유효하지 않은 토큰입니다.");
+    WRONG_TOKEN(403, "AT-0001", "유효하지 않은 토큰입니다."),
+    TOKEN_EXPIRED(401, "AT-0002", "토큰이 만료되었습니다.");
 
     final int code;
     final String errorCode;
