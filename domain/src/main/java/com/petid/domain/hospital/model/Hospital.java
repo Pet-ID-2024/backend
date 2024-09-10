6c8dc817807f6efc925d361bff5e5648e2f875a0
HyeongMokJeong <jhm1062@naver.com>
2024-09-10 14:26:15 +0900

    Chore : 병원 이미지 List 변경

--- domain/src/main/java/com/petid/domain/hospital/model/Hospital.java
+++ domain/src/main/java/com/petid/domain/hospital/model/Hospital.java
@@ -1,11 +1,13 @@
 package com.petid.domain.hospital.model;
 
+import java.util.List;
+
 public record Hospital(
         Long id,
         long sidoId,
         long sigunguId,
         long eupmundongId,
-        String imageUrl,
+        List<String> imageUrl,
         String address,
         String name,
         String hours,
