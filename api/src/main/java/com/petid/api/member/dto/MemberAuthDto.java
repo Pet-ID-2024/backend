7057ecb281d8445a1c6b0eccf246b3b0e52f757a
HyeongMokJeong <jhm1062@naver.com>
2024-09-23 16:38:47 +0900

    Chore : 회원 정보 관련 주민번호 항목 제외

--- api/src/main/java/com/petid/api/member/dto/MemberAuthDto.java
+++ api/src/main/java/com/petid/api/member/dto/MemberAuthDto.java
@@ -6,7 +6,6 @@ import com.petid.domain.member.model.MemberAuth;
 public record MemberAuthDto() {
     public record Request(
             String name,
-            String ssn,
             String address,
             String phone
     ) {
@@ -15,7 +14,7 @@ public record MemberAuthDto() {
                     null,
                     member.uid(),
                     name,
-                    ssn,
+                    null,
                     address,
                     phone
             );
@@ -23,19 +22,13 @@ public record MemberAuthDto() {
     }
 
     public record Response(
-            Long id,
-            String uid,
             String name,
-            String ssn,
             String address,
             String phone
     ) {
         public static Response from(MemberAuth memberAuth) {
             return new Response(
-                    memberAuth.id(),
-                    memberAuth.uid(),
                     memberAuth.name(),
-                    memberAuth.ssn(),
                     memberAuth.address(),
                     memberAuth.phone()
             );
