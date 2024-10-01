ca6621efedc1748df1c85bcc908beec99ceb64ce
HyeongMokJeong <jhm1062@naver.com>
2024-10-01 14:08:52 +0900

    Chore : NotNull 조건 수정

--- core/src/main/resources/db/migration/V2__create_member_auth_table.sql
+++ core/src/main/resources/db/migration/V2__create_member_auth_table.sql
@@ -3,9 +3,9 @@ CREATE TABLE member_auth
     id        BIGINT AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT       NOT NULL,
     name      VARCHAR(10)  NOT NULL,
-    ssn       VARCHAR(255) NOT NULL,
-    phone     VARCHAR(20)  NOT NULL,
-    address   VARCHAR(255) NOT NULL,
+    ssn       VARCHAR(255),
+    phone     VARCHAR(20),
+    address   VARCHAR(255),
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
