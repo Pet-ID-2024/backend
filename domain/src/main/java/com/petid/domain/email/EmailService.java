03e300b88da8dc7b2a8b3c454f9d3232a48c18fd
HyeongMokJeong <jhm1062@naver.com>
2024-11-27 16:30:02 +0900

    Chore : 환경변수 의존성 수정

--- domain/src/main/java/com/petid/domain/email/EmailService.java
+++ domain/src/main/java/com/petid/domain/email/EmailService.java
@@ -1,6 +1,7 @@
 package com.petid.domain.email;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
+    @Value("${spring.mail.username}")
+    private String emailSenderAddr;
+
     private final JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String text) {
+    public void sendEmail(String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
+        message.setTo(emailSenderAddr);
         message.setSubject(subject);
         message.setText(text);
         mailSender.send(message);
