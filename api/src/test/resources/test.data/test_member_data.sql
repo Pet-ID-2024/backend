b687f4693bef821eebbe92594ca109f635fbbb6c
HyeongMokJeong <jhm1062@naver.com>
2024-12-18 15:13:10 +0900

    Test: 테스트 데이터 DATE 타입에 맞게 수정

--- api/src/test/resources/test.data/test_member_data.sql
+++ api/src/test/resources/test.data/test_member_data.sql
@@ -1,6 +1,6 @@
 INSERT INTO member (id, uid, platform, email, role, fcm_token, created_at, updated_at, status, status_update_date)
 VALUES
-    (1, 'test', 'google', 'test@gamil.com', 'ROLE_USER', 'test', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08 00:00:00'),
-    (2, 'test2', 'google', 'test2@gamil.com', 'ROLE_USER', 'test2', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08 00:00:00'),
-    (3, 'test3', 'kakao', 'test3@gamil.com', 'ROLE_USER', 'test3', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08 00:00:00')
+    (1, 'test', 'google', 'test@gamil.com', 'ROLE_USER', 'test', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08'),
+    (2, 'test2', 'google', 'test2@gamil.com', 'ROLE_USER', 'test2', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08'),
+    (3, 'test3', 'kakao', 'test3@gamil.com', 'ROLE_USER', 'test3', '2024-06-08 00:00:00', '2024-09-06 05:29:00', 'NORMAL', '2024-06-08')
 ;
